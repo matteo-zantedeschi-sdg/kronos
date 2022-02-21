75333a148518bf026dcf8d5362d349075ecc7c97
LeLoMAu <35969531+LeLoMAu@users.noreply.github.com>
2022-02-21 17:38:31 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='kronos',
+    version='0.0.1',
+    packages=['kronos', 'kronos.models'],
+    url='',
+    license='SDG Group',
+    author='zantedeschim',
+    author_email='matteo.zantedeschi@sdggroup.com',
+    description='Kronos package to manage time-series in Databricks'
+)
