f8d9fa718fe0b7e9cd1cbe4438fdb2d5d26de678
elvira-bruno-sdg <elvira.bruno@sdggroup.com>
2022-09-21 12:17:38 +0200

    changed version to create wheel

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='kronos',
-    version='2.0.1',
+    version='2.0.2',
     packages=['kronos', 'kronos.models'],
     author='zantedeschim',
     author_email='matteo.zantedeschi@sdggroup.com',
