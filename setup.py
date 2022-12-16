4b8f85a1a770c6e06140d6602386d490c440084a
elvira-bruno-sdg <elvira.bruno@sdggroup.com>
2022-12-16 10:40:08 +0100

    wheel version 2.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='kronos',
-    version='2.0.5',
+    version='2.0.6',
     packages=['kronos', 'kronos.models'],
     author='zantedeschim',
     author_email='matteo.zantedeschi@sdggroup.com',
